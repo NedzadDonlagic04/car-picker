2ac8775aa4d8972331f5db4e7fcbebbdf36237b5
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-09-26 17:21:18 +0200

    Added a box with a shadow
    It will store the pictures of the cars and their
    respective images.

---
+++ src/components/Card.vue
@@ -0,0 +1,27 @@
+<script>
+    import Image from './Image.vue';
+    import Info from './Info.vue';
+
+    export default {
+        components: { Image, Info }
+    };
+</script>
+
+<template>
+    <div class="card">
+        <Image />
+        <Info />
+    </div>
+</template>
+
+<style scoped>
+    .card {
+        --shadow-color: hsl(0, 10%, 30%);
+
+        border: 3px solid black;
+        border-radius: 5px;
+        box-shadow: -10px -10px 15px var(--shadow-color);
+        height: 500px;
+        width: 400px;
+    }
+</style>
