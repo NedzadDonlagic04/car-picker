1df3a718716fc9f592a9abc386019202c3a77645
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-09-26 17:42:05 +0200

    Gave the component it's space and background color

---
+++ src/components/Info.vue
@@ -0,0 +1,18 @@
+<script>
+    export default {
+
+    };
+</script>
+
+<template>
+    <div class="info">
+        
+    </div>
+</template>
+
+<style scoped>
+    .info {
+        height: 60%;
+        background-color: hsl(120, 80%, 40%);
+    }
+</style>
