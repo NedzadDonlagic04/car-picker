5dba63260d46538936b50dcfc7456aeb8d74ecd8
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-09-26 17:46:26 +0200

    Gave the image a proper size and functionality
    The component has a prop which will take a string
    which will determine what image is displayed on
    the card.

---
+++ src/components/Image.vue
@@ -0,0 +1,31 @@
+<script>
+    export default {
+        props: {
+            show: {
+                type: String,
+                default: 'black'
+            }
+        }
+    };
+</script>
+
+<template>
+    <div class="images">
+        <img v-if="show === 'black'" src="./../assets/black-car.jpeg" alt="black-car">
+        <img v-else-if="show === 'gray'" src="./../assets/gray-car.jpg" alt="gray-car">
+        <img v-else src="./../assets/red-car.jpg" alt="red-car">
+    </div>
+</template>
+
+<style scoped>
+    .images {
+        height: 40%;
+        border-bottom: 2px solid black;
+        display: flex;
+        justify-content: center;
+    }
+
+    img {
+        max-height: 100%;
+    }
+</style>
